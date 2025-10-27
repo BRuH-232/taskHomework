e632773b0474cbc77ae6ab49f55e8d1b8f14b012
BRuH-232 <dimavaluev771@gmail.com>
2025-10-27 23:01:45 +0300

    gotovo

---
+++ task-list/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1 class="site-description">Список дел</h1>
+
+    <div class="tasks">
+      <TaskItem
+        v-for="task in tasks" 
+        :task="task"
+      />
+    </div>
+
+  </div>
+</template>
+
+
+<script setup lang="ts">
+//import { ref, computed } from "vue";
+import TaskItem from "./components/TaskItem.vue";
+
+const tasks = [
+  { _id: "1", title: "Убока дома:", text: "Убрать квартиру: помыть полы, \
+  протереть пыль, пропылесосить."},
+  { _id: "2", title: "Покупка продуктов:", text: "Купить продукты: макароны, помидоры, \
+  морковь, свинной фарш."},
+  { _id: "3", title: "Готовка ужина:", text: "Приготовить макароны со свинным фаршом \
+  под томатным соусом."},
+  { _id: "4", title: "Подготовка ко сну:", text: "Выполнить стандартные ритуалы: \
+  помыться, побриться, лечь под одеялко."},
+];
+
+</script>
+
+<style>
+.site-description{
+  height: 70px;
+  width: 350px;
+  border-radius: 15px;
+  box-shadow: 0px 7px 5px 0px #ccc;
+  margin: 40px auto;
+}
+.tasks {
+  display: flex;
+  gap: 16px;
+  flex-direction: column;
+}
+</style>
---
+++ task-list/src/components/TaskItem.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="task-card">
+    <header>
+      <div class="task-name"><b>{{ props.task.title }}</b></div>
+    </header>
+
+    <main>
+      <div class="task-description">{{ props.task.text }}</div>
+    </main>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  task: { title: string; text: string };
+}>();
+
+
+</script>
+
+<style>
+.task-card {
+  display: flex;
+  gap: 20px;
+  flex-direction: row;
+  border: 3px solid #ccc;
+  padding: 12px;
+  border-radius: 30px;
+  box-shadow: 2px 2px 9px 1px #727;
+}
+.task-name{
+  font-size: 28px;
+  padding: 20px;
+}
+.task-description{
+  width: 300px;
+  margin: 5px;
+  padding: 15px; 
+  border: 3px solid #ccc;
+  border-radius: 15px;
+  box-shadow: 2px 2px 9px 1px #727;
+}
+</style>
